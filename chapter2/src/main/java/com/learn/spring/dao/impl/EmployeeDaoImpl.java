951e5f0521f20d6ede1bd108a5f0f5469011675a
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-03-27 16:21:46 +0300

    Additional bean for property injection

--- chapter2/src/main/java/com/learn/spring/dao/impl/EmployeeDaoImpl.java
+++ chapter2/src/main/java/com/learn/spring/dao/impl/EmployeeDaoImpl.java
@@ -5,18 +5,27 @@ import com.learn.spring.model.Employee;
 
 public class EmployeeDaoImpl implements EmployeeDao {
 
-    private Employee employee;
+    private Employee employee1;
+    private Employee employee2;
 
     @Override
     public Employee getEmployeeFromDb() {
-        return employee;
+        return employee1;
     }
 
-    public Employee getEmployee() {
-        return employee;
+    public Employee getEmployee1() {
+        return employee1;
     }
 
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
+    public void setEmployee1(Employee employee1) {
+        this.employee1 = employee1;
+    }
+
+    public Employee getEmployee2() {
+        return employee2;
+    }
+
+    public void setEmployee2(Employee employee2) {
+        this.employee2 = employee2;
     }
 }
