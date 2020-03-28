e381e2e6f4dbb216caa297630a0b5de700325614
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-03-28 12:39:27 +0300

    Chapter 4 - EmployeeService implementation

--- chapter4/src/main/java/com/learn/spring/Main.java
+++ chapter4/src/main/java/com/learn/spring/Main.java
@@ -1,6 +1,5 @@
 package com.learn.spring;
 
-import com.learn.spring.dao.EmployeeDao;
 import com.learn.spring.entity.Employee;
 import com.learn.spring.service.EmployeeService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -37,7 +36,7 @@ public class Main {
     //>>> After insert
     //Hibernate: select employee0_.id as id1_0_, employee0_.department as departme2_0_, employee0_.first_name as first_na3_0_, employee0_.job_title as job_titl4_0_, employee0_.last_name as last_nam5_0_, employee0_.salary as salary6_0_ from employee employee0_
     //Employee(id=1, firstName=Dmitrii, lastName=Cheremisin, jobTitle=Software Developer, department=IT, salary=100000)
-    //Employee(id=2, firstName=Irina, lastName=Cheremisina, jobTitle=CEO, department=Administrative, salary=200000)
+    //Employee(id=2, firstName=Irina, lastName=Cheremisina, jobTitle=CEO, department=Administration, salary=200000)
     //Employee(id=3, firstName=Vasya, lastName=Pupkin, jobTitle=Cleaner, department=Office stuff, salary=40000)
     // ========================
 
